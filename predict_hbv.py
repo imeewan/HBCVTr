4cc6039022e2ca021dccd21054c17623bdc00af1
imeewan <itdelight47@gmail.com>
2023-12-04 16:15:32 +0700

    Update predict_hbv.py

--- predict_hbv.py
+++ predict_hbv.py
@@ -36,44 +36,57 @@ from pretrained_utils import *
 from rdkit import Chem
 from rdkit.Chem import SaltRemover
 
+
 if __name__ == "__main__":
-        
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    smiles = input("Enter the SMILES string of the compound: ")
+
+    virus_choice = input("Do you want to predict the compound's activity against HBV or HCV? (Enter HBV or HCV): ").lower()
+
+    print("Analysis in progress ...")
+
+    if virus_choice == 'hbv':
+        model_path = "model/hbv_model.pt"
+        max_pact = max_pact_hbv
+        min_pact = min_pact_hbv
+    elif virus_choice == 'hcv':
+        model_path = "model/hcv_model.pt"
+        max_pact = max_pact_hcv
+        min_pact = min_pact_hcv
+    else:
+        raise ValueError("Invalid input. Please enter either 'HBV' or 'HCV'.")
+
     max_length = 250
     
-    
     model = DualBartModel(config1, config2, reg_mod)
-    model.load_state_dict(torch.load("model/hbv_model.pt", map_location=device))
+    model.load_state_dict(torch.load(model_path, map_location=device))
     model.to(device)
     
-    #SMILES for the prediction
-    smiles = 'Nc1ccn([C@@H]2CC(O)[C@H](CO)O2)c(=O)n1'
-    
-    smiles_data_no_salt = remove_salt(smiles) 
+    smiles_data_no_salt = remove_salt(smiles)
     smiles = smiles_data_no_salt
-    
+
     input_encoding1 = tokenizer1.encode_plus(smiles, truncation=True, max_length=max_length, padding='max_length', return_tensors="pt")
     input_encoding2 = tokenizer2.encode_plus(smiles, truncation=True, max_length=max_length, padding='max_length', return_tensors="pt")
-    
+
     input_ids1 = input_encoding1['input_ids'].to(device)
     attention_mask1 = input_encoding1['attention_mask'].to(device)
     input_ids2 = input_encoding2['input_ids'].to(device)
     attention_mask2 = input_encoding2['attention_mask'].to(device)
     
+
     with torch.no_grad():
         output = model(input_ids1=input_ids1, attention_mask1=attention_mask1,
                        input_ids2=input_ids2, attention_mask2=attention_mask2)
     
-    
     prediction = output
-    
     prediction_value = prediction.cpu().numpy()[0]
-    
-    print('Predicted pACT: ', prediction_value * (max_pact_hbv - min_pact_hbv) + min_pact_hbv)
-    predicted_EC50 = 10**-(prediction_value * (max_pact_hbv - min_pact_hbv) + min_pact_hbv) * 10**9
+    print('SMILES: ', smiles)
+    print('Predicted pACT: ', prediction_value * (max_pact - min_pact) + min_pact)
+    predicted_EC50 = 10**-(prediction_value * (max_pact - min_pact) + min_pact) * 10**9
     print('Predicted EC50 :', predicted_EC50, 'nM')
-    
+
 
 # In[ ]:
 
