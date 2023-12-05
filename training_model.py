ad11e3e48dc1fee1267b9664dcf406d749d51c51
imeewan <itdelight47@gmail.com>
2023-12-05 14:56:30 +0700

    Update training_model.py

--- training_model.py
+++ training_model.py
@@ -3,7 +3,8 @@
 
 # In[1]:
 
-
+                
+                
 from BartDataset import BartDataset
 from CustomBart_Atomic_Tokenizer import CustomBart_Atomic_Tokenizer
 from CustomBart_FG_Tokenizer import CustomBart_FG_Tokenizer
@@ -33,32 +34,7 @@ import json
 import os
 from utils import *
 
-
-
-if __name__ == "__main__":
-
-    max_length = 250
-    batch_size = 8
-
-    data_path = "data/hbv_dataset.csv"
-    train_dataloader, val_dataloader = train_val_proc(data_path)
-
-    d_models = [128]
-    encoder_ffn_dims = [256]
-    num_attention_heads = [8]
-    num_hidden_layers = [2]
-    dropouts = [0.15]
-    learning_rates = [1e-6] 
-    reg_mod = [256, 128]
-    weight_decay = 0.001
-    num_epochs = 5
-
-    param_combinations = list(itertools.product(d_models, encoder_ffn_dims, num_attention_heads, num_hidden_layers, dropouts, learning_rates))
-    combinations = param_combinations
-
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-
+def training_model(combinations):
     for idx, (d_model1, encoder_ffn_dim1, num_attention_heads1, num_hidden_layers1, dropout1, lr1) in enumerate(combinations):
 
         (d_model2, encoder_ffn_dim2, num_attention_heads2, num_hidden_layers2, dropout2, lr2) = (d_model1, encoder_ffn_dim1, num_attention_heads1, num_hidden_layers1, dropout1, lr1)
@@ -97,7 +73,7 @@ if __name__ == "__main__":
         print(f"d_model1: {d_model1}, encoder_ffn_dim1: {encoder_ffn_dim1}, num_attention_heads1: {num_attention_heads1}, num_hidden_layers1: {num_hidden_layers1}")
         print(f"d_model2: {d_model2}, encoder_ffn_dim2: {encoder_ffn_dim2}, num_attention_heads2: {num_attention_heads2}, num_hidden_layers2: {num_hidden_layers2}")
 
-        log_file_path = f"model/hcv_model.log"
+        log_file_path = f"model/new_model2.log"
 
         os.makedirs(os.path.dirname(log_file_path), exist_ok=True)
 
@@ -177,10 +153,34 @@ if __name__ == "__main__":
                 outfile.write(json.dumps(log_dict) + '\n')
 
             if avg_val_r2 > max_r2:
-                torch.save(model.state_dict(), f"model/new_model.pt")
+                torch.save(model.state_dict(), f"model/new_model2.pt")
                 max_r2 = avg_val_r2
 
                 
+if __name__ == "__main__":
+
+    max_length = 250
+    batch_size = 16
+
+    data_path = "data/hbv_dataset.csv"
+    train_dataloader, val_dataloader = train_val_proc(data_path)
+
+    d_models = [16]
+    encoder_ffn_dims = [16]
+    num_attention_heads = [2]
+    num_hidden_layers = [1]
+    dropouts = [0.15]
+    learning_rates = [1e-6] 
+    reg_mod = [32, 32]
+    weight_decay = 0.001
+    num_epochs = 2
+
+    param_combinations = list(itertools.product(d_models, encoder_ffn_dims, num_attention_heads, num_hidden_layers, dropouts, learning_rates))
+    combinations = param_combinations
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    training_model(combinations)          
 
 # In[3]:
 
